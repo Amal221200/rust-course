155187fbe2b945b26f91db3f6ac5da7f270cc9bc
Amal221200 <amalmurikkoli2000@gmail.com>
2024-02-09 15:54:15 +0530

    02 variables, shadowing, constants

---
+++ 02 Variables, Shadowing, Constants/proj/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let mut x: i32 = 5;
+    // x= 34;
+    // println!("The value of x is {x}", x = x);
+    // {
+    //     let x = 8;
+    //     println!("The value of x is {x}", x = x);
+    // }
+    // let x: &str= "Hello World!";
+    // x = "Hello World!";
+    let num;
+    num = 48;
+    const PI: f64 = 3.14;
+    // const PI: f64 = 355;
+    println!("The value of x is {PI}", PI = num);
+}
